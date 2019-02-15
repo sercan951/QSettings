9e9b7c02e710724070c7c17ccb292e616ff22ba9
Sercan951 <47636900+sercan951@users.noreply.github.com>
2019-02-15 10:06:00 +0300

    Add files via upload

---
+++ MySettings.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2019-02-14T12:30:36
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = MySettings
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        dialog.cpp
+
+HEADERS  += dialog.h
+
+FORMS    += dialog.ui
---
+++ dialog.cpp
@@ -0,0 +1,61 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+
+Dialog::Dialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+
+    LoadSettings(); // when the window is show up call LoadSettings function.
+
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+
+}
+
+void Dialog::SaveSettings()
+{
+    //Save
+    QSettings setting("MyApp","mysetting");  //MyApp = name, mysetting = subname
+    setting.beginGroup("MainWindow");
+    setting.setValue("position",this->geometry());
+    setting.endGroup();
+
+    qDebug() << "Saved";
+
+}
+
+void Dialog::LoadSettings()
+{
+    //Load
+    QSettings setting("MyApp","mysetting");  //MyApp = name, mysetting = subname
+    setting.beginGroup("MainWindow");
+    QRect myrect = setting.value("position").toRect();
+    setGeometry(myrect);
+    setting.endGroup();
+
+    qDebug() << "Loaded";
+    //dsdfsd
+
+}
+
+
+void Dialog::on_pushButton_clicked()
+{
+    //Save
+    SaveSettings(); //reason to create savesettings seperately is called disagn. we can call savesettings from everything
+    //but calling on_pushbutton_clicked is a bit weird and outdate.
+
+}
+
+void Dialog::on_pushButton_2_clicked()
+{
+    //Load
+    LoadSettings();
+
+}
+
